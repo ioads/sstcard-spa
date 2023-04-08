83cc199ede37435b9e3ded891354e483eb0f6308
Iago Oliveira <iago.oliveira@quality.com.br>
2023-04-08 10:20:14 -0300

    Tela de login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,164 +1,70 @@
 <template>
-    <section id="profile">
-      <mdb-row>
-        <mdb-col md="3">
-          <mdb-card cascade narrow>
-            <mdb-view>
-              <img src="https://mdbootstrap.com/img/Photos/Horizontal/People/6-col/img%20%283%29.jpg" alt="Project" class="img-fluid"/>
-              <mdb-mask overlay="white-slight" waves/>
+    <section id="maps">
+      <mdb-row class="justify-content-center">
+        <mdb-col md="5">
+          <mdb-card cascade narrow class="mt-5">
+            <mdb-view class="gradient-card-header blue darken-2">
+              <h4 class="h4-responsive text-white">
+                Login
+              </h4>
             </mdb-view>
             <mdb-card-body class="text-center">
-              <mdb-card-title class="font-bold mb-2">
-                <strong>Alice Mayer</strong>
-              </mdb-card-title>
-              <h5 class="indigo-text">
-                <strong>Photographer</strong>
-              </h5>
-              <h6 class="text-justify">
-                <strong>About:</strong>
-              </h6>
-              <p class="text-justify">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione perferendis quod animi dignissimos consectetur quibusdam numquam laboriosam, minus, provident...</p>
-              <div class="text-right">
-                <mdb-btn outline="primary" rounded size="sm">More...</mdb-btn>
-              </div>
+                    <form>
+                        <!-- Email input -->
+                        <mdb-input
+                        type="email"
+                        label="E-mail"
+                        id="form1Email"
+                        v-model="email"
+                        wrapperClass="mb-4"
+                        />
+                        <!-- Password input -->
+                        <mdb-input
+                        type="password"
+                        label="Senha"
+                        id="form1Password"
+                        v-model="password"
+                        wrapperClass="mb-4"
+                        />
+                        <!-- 2 column grid layout for inline styling -->
+                        <mdb-row class="mb-4">
+                        <mdb-col class="d-flex justify-content-center">
+                            <!-- Checkbox -->
+                            <MDBCheckbox
+                            label="Remember me"
+                            id="form1LoginCheck"
+                            v-model="form1LoginCheck"
+                            wrapperClass="mb-3 mb-md-0"
+                            />
+                        </mdb-col>
+                        <mdb-col>
+                            <!-- Simple link -->
+                            <a href="#!">Esqueceu a senha?</a>
+                        </mdb-col>
+                        </mdb-row>
+                        <!-- Submit button -->
+                        <mdb-btn color="primary" block> Entrar </mdb-btn>
+                    </form>
             </mdb-card-body>
           </mdb-card>
-        </mdb-col>
-        <mdb-col md="9">
-          <section class="text-center pb-3">
-            <mdb-row class="d-flex justify-content-center">
-              <mdb-col lg="6" xl="5" class="mb-3">
-                <mdb-card cascade narrow class="d-flex mb-5">
-                  <mdb-view>
-                    <img src="https://mdbootstrap.com/img/Mockups/Horizontal/6-col/pro-profile-page.jpg" alt="Project" class="img-fluid"/>
-                    <mdb-mask overlay="white-slight" waves/>
-                  </mdb-view>
-                  <mdb-card-body>
-                    <mdb-card-title class="font-bold mb-3">
-                      <strong>Project name</strong>
-                    </mdb-card-title>
-                    <mdb-card-text>Some quick example text to build on the card title and make up the bulk of the card's content.</mdb-card-text>
-                  </mdb-card-body>
-                  <mdb-card-footer class="links-light profile-card-footer">
-                    <span class="right">
-                      <a class="p-2" href="#profile">
-                        Live Preview
-                        <mdb-icon icon="photo" class="ml-1"/>
-                      </a>
-                    </span>
-                  </mdb-card-footer>
-                </mdb-card>
-              </mdb-col>
-              <mdb-col lg="6" xl="5" class="mb-3">
-                <mdb-card cascade narrow class="d-flex mb-5">
-                  <mdb-view>
-                    <img src="https://mdbootstrap.com/img/Mockups/Horizontal/6-col/pro-signup.jpg" alt="Project" class="img-fluid"/>
-                    <mdb-mask overlay="white-slight" waves/>
-                  </mdb-view>
-                  <mdb-card-body>
-                    <mdb-card-title class="font-bold mb-3">
-                      <strong>Project name</strong>
-                    </mdb-card-title>
-                    <mdb-card-text>Some quick example text to build on the card title and make up the bulk of the card's content.</mdb-card-text>
-                  </mdb-card-body>
-                  <mdb-card-footer class="links-light profile-card-footer">
-                    <span class="right">
-                      <a class="p-2" href="#profile">
-                        Live Preview
-                        <mdb-icon icon="photo" class="ml-1"/>
-                      </a>
-                    </span>
-                  </mdb-card-footer>
-                </mdb-card>
-              </mdb-col>
-            </mdb-row>
-            <mdb-row class="d-flex justify-content-center">
-              <mdb-col lg="6" xl="5" class="mb-3">
-                <mdb-card cascade narrow class="d-flex mb-5">
-                  <mdb-view>
-                    <img src="https://mdbootstrap.com/img/Mockups/Horizontal/6-col/pro-pricing.jpg" alt="Project" class="img-fluid"/>
-                    <mask overlay="white-slight" waves/>
-                  </mdb-view>
-                  <mdb-card-body>
-                    <mdb-card-title class="font-bold mb-3">
-                      <strong>Project name</strong>
-                    </mdb-card-title>
-                    <mdb-card-text>Some quick example text to build on the card title and make up the bulk of the card's content.</mdb-card-text>
-                  </mdb-card-body>
-                  <mdb-card-footer class="links-light profile-card-footer">
-                    <span class="right">
-                      <a class="p-2" href="#profile">
-                        Live Preview
-                        <mdb-icon icon="photo" class="ml-1"/>
-                      </a>
-                    </span>
-                  </mdb-card-footer>
-                </mdb-card>
-              </mdb-col>
-              <mdb-col lg="6" xl="5" class="mb-3">
-                <mdb-card cascade narrow class="d-flex mb-5">
-                  <mdb-view>
-                    <img src="https://mdbootstrap.com/img/Mockups/Horizontal/6-col/pro-landing.jpg" alt="Project" class="img-fluid"/>
-                    <mask overlay="white-slight" waves/>
-                  </mdb-view>
-                  <mdb-card-body>
-                    <mdb-card-title class="font-bold mb-3">
-                      <strong>Project name</strong>
-                    </mdb-card-title>
-                    <mdb-card-text>Some quick example text to build on the card title and make up the bulk of the card's content.</mdb-card-text>
-                  </mdb-card-body>
-                  <mdb-card-footer class="links-light profile-card-footer">
-                    <span class="right">
-                      <a class="p-2" href="#profile">
-                        Live Preview
-                        <mdb-icon icon="photo" class="ml-1"/>
-                      </a>
-                    </span>
-                  </mdb-card-footer>
-                </mdb-card>
-              </mdb-col>
-              <mdb-col lg="12">
-                <div class="text-center">
-                  <mdb-pagination circle color="blue">
-                    <mdb-page-item disabled>First</mdb-page-item>
-                    <mdb-page-nav prev></mdb-page-nav>
-                    <mdb-page-item active>1</mdb-page-item>
-                    <mdb-page-item>2</mdb-page-item>
-                    <mdb-page-item>3</mdb-page-item>
-                    <mdb-page-item>4</mdb-page-item>
-                    <mdb-page-item>5</mdb-page-item>
-                    <mdb-page-nav next></mdb-page-nav>
-                    <mdb-page-item disabled>Last</mdb-page-item>
-                  </mdb-pagination>
-                </div>
-              </mdb-col>
-            </mdb-row>
-          </section>
         </mdb-col>
       </mdb-row>
     </section>
   </template>
   
   <script>
-  import { mdbRow, mdbCol, mdbCard, mdbCardBody, mdbView, mdbMask, mdbCardTitle, mdbCardText, mdbCardFooter, mdbIcon, mdbBtn, mdbPagination, mdbPageNav, mdbPageItem } from 'mdbvue'
+  import { mdbRow, mdbCol, mdbCard, mdbView, mdbCardBody, mdbInput, mdbBtn } from 'mdbvue'
   
   export default {
-    name: 'Profile',
+    name: 'Maps',
     components: {
       mdbRow,
+      mdbInput,
       mdbCol,
       mdbCard,
-      mdbCardBody,
       mdbView,
-      mdbMask,
-      mdbCardTitle,
-      mdbCardText,
-      mdbCardFooter,
-      mdbIcon,
-      mdbBtn,
-      mdbPagination,
-      mdbPageNav,
-      mdbPageItem
+      mdbCardBody,
+      mdbBtn
     },
     data () {
       return {
@@ -167,13 +73,14 @@
   }
   </script>
   
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
   <style scoped>
-  .profile-card-footer {
-    background-color: #F7F7F7 !important;
-    padding: 1.25rem;
+  .card.card-cascade .view.gradient-card-header {
+    padding: 1rem 1rem;
+    text-align: center;
   }
-  .card.card-cascade .view {
-    box-shadow: 0 3px 10px 0 rgba(0, 0, 0, 0.15), 0 3px 12px 0 rgba(0, 0, 0, 0.15);
+  .card.card-cascade h3, .card.card-cascade h4 {
+    margin-bottom: 0px;
   }
   </style>
   
